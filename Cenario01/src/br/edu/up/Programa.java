24d0c732ec0c23910add572c9bbf9476cc9f9904
João Pedro <jpjoaopedro201444@gmail.com>
2024-04-15 21:32:54 -0300

    Instanciamento Livros

--- Cenario01/src/br/edu/up/Programa.java
+++ Cenario01/src/br/edu/up/Programa.java
@@ -1,6 +1,24 @@
 package br.edu.up;
+
+import br.edu.up.modelos.Livro;
+
 public class Programa {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+    public static void main(String[] args) {
+
+        // instanciamento do livro 1
+        Livro livro1 = new Livro();
+        livro1.codigo = "1598FHK";
+        livro1.titulo = "Core Java 2";
+        livro1.autores = "Cay S. Horstmann e Gary Cornell";
+        livro1.isbn = "0130819336";
+        livro1.ano = 2005;
+        // instanciamento do livro 2
+        Livro livro2 = new Livro();
+        livro2.codigo = "9865PLO";
+        livro2.titulo = "Java, Como programar";
+        livro2.autores = "Harvey Deitel";
+        livro2.isbn = "0130341517";
+        livro2.ano = 2015;
     }
 }
---
+++ Cenario01/src/br/edu/up/modelos/Livro.java
@@ -0,0 +1,50 @@
+package br.edu.up.modelos;
+
+public class Livro {
+    public String codigo;
+    public String titulo;
+    public String autores;
+    public String isbn;
+    public int ano;
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutores() {
+        return autores;
+    }
+
+    public void setAutores(String autores) {
+        this.autores = autores;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public int getAno() {
+        return ano;
+    }
+
+    public void setAno(int ano) {
+        this.ano = ano;
+    }
+
+}
