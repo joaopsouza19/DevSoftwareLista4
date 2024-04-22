d6f68170333999a355b412b529a2710888783fae
Joao <jpjoaopedro201444@gmail.com>
2024-04-22 20:25:19 -0300

    Criação ponto1 e ponto2

---
+++ Cenario02/src/br/edu/up/Programa.java
@@ -0,0 +1,13 @@
+package br.edu.up;
+
+import br.edu.up.modelos.Ponto;
+
+public class Programa {
+    public static void main(String[] args) {
+
+        Ponto ponto1 = new Ponto();
+
+        Ponto ponto2 = new Ponto(2, 5);
+
+    }
+}
