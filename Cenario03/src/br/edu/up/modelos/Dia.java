35856c67e1ef3650b904a6b9fc3384c6f2fb9dcf
Joao <jpjoaopedro201444@gmail.com>
2024-04-22 21:36:43 -0300

    Dia

---
+++ Cenario03/src/br/edu/up/modelos/Dia.java
@@ -0,0 +1,11 @@
+package br.edu.up.modelos;
+
+public class Dia {
+    public int diaMes;
+    public Compromisso[] compromissos;
+
+    public Dia(int diaMes) {
+        this.diaMes = diaMes;
+    }
+
+}
