4903c0e64e054c651e96559fc2ac18a8f44cb9f9
Joao <jpjoaopedro201444@gmail.com>
2024-05-08 15:31:55 -0300

    Get/set Mes

--- Cenario03/src/br/edu/up/modelos/Mes.java
+++ Cenario03/src/br/edu/up/modelos/Mes.java
@@ -11,4 +11,37 @@ public class Mes {
             dias[i] = new Dia(i + 1);
         }
     }
+
+    public void adicionarCompromisso(Compromisso comp, int diaMes){
+        if (diaMes >= 1 && diaMes <= qtdeDias){
+            dias[diaMes - 1].adicionarCompromisso(comp);
+        } else {
+            System.out.println("Dia inválido para adicionar compromisso.");
+        }
+    }
+
+    public void excluirCompromisso(int diaMes, int hora){
+        if (diaMes >= 1 && diaMes <= qtdeDias){
+            dias[diaMes - 1].excluirCompromisso(hora);
+        } else {
+            System.out.println("Dia inválido para excluir compromisso.");
+        }
+    }
+
+    public void listarCompromissos(int diaMes){
+        if (diaMes >= 1 && diaMes <= qtdeDias){
+            dias[diaMes - 1].listarCompromissos();
+        } else {
+            System.out.println("Dia inválido para listar compromissos.");
+        }
+    }
+
+    public void listarCompromissos(){
+        System.out.println("Compromissos do mês:");
+        for (int i = 0; i < qtdeDias; i++){
+            System.out.println("Dia " + (i + 1) + ":");
+            dias[i].listarCompromissos();
+        }
+    }
+    
 }
