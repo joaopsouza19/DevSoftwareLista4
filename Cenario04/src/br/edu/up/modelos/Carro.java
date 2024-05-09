9f85531ce942baebaa7f8b6d8bff9a87fcff6c5b
Joao <jpjoaopedro201444@gmail.com>
2024-05-09 15:35:31 -0300

    Get/setters + emitirRelatorio

--- Cenario04/src/br/edu/up/modelos/Carro.java
+++ Cenario04/src/br/edu/up/modelos/Carro.java
@@ -5,33 +5,33 @@ public class Carro {
     private String placa;
     private String cor;
 
-    public Carro(String modelo, String placa, String cor) {
+    public Carro(String modelo, String placa, String cor){
         this.modelo = modelo;
         this.placa = placa;
         this.cor = cor;
     }
 
-    public String getModelo() {
+    public String getModelo(){
         return modelo;
     }
 
-    public void setModelo(String modelo) {
+    public void setModelo(String modelo){
         this.modelo = modelo;
     }
 
-    public String getPlaca() {
+    public String getPlaca(){
         return placa;
     }
 
-    public void setPlaca(String placa) {
+    public void setPlaca(String placa){
         this.placa = placa;
     }
 
-    public String getCor() {
+    public String getCor(){
         return cor;
     }
 
-    public void setCor(String cor) {
+    public void setCor(String cor){
         this.cor = cor;
     }
 }
--- Cenario04/src/br/edu/up/modelos/ControleEstacionamento.java
+++ Cenario04/src/br/edu/up/modelos/ControleEstacionamento.java
@@ -7,8 +7,40 @@ public class ControleEstacionamento {
     private List<Carro> carrosEstacionados;
     private int vagasDisponiveis;
 
-    public ControleEstacionamento(int numeroVagas) {
+    public ControleEstacionamento(int numeroVagas){
         carrosEstacionados = new ArrayList<>();
         vagasDisponiveis = numeroVagas;
+    }
+
+    public void entradaCarro(Carro carro){
+        if (vagasDisponiveis > 0){
+            carrosEstacionados.add(carro);
+            vagasDisponiveis--;
+            System.out.println("O carro foi estacionado com sucesso!");
+        } else {
+            System.out.println("Não possui vagas disponíveis.");
+        }
+    }
+
+    public void saidaCarro(String placa){
+        boolean encontrado = false;
+        for (Carro carro : carrosEstacionados){
+            if (carro.getPlaca().equals(placa)){
+                carrosEstacionados.remove(carro);
+                vagasDisponiveis++;
+                encontrado = true;
+                System.out.println("O carro foi retirado com sucesso!");
+                break;
+            }
+        }
+        if (!encontrado){
+            System.out.println("O carro não foi encontrado no estacionamento.");
+        }
+    }
+
+    public void emitirRelatorio(){
+        System.out.println("O número de carros estacionados: " + carrosEstacionados.size());
+        System.out.println("O número de vagas disponíveis: " + vagasDisponiveis);
+        System.out.println("O número de valor arrecadado: R$ " + (carrosEstacionados.size() * 5));
     }
 }
