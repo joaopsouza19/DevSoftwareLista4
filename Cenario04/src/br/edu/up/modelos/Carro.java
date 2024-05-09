bf24e8293eb1f813bcb781d50ba414ea06d30941
Joao <jpjoaopedro201444@gmail.com>
2024-05-09 15:18:15 -0300

    Carro

---
+++ Cenario04/src/br/edu/up/modelos/Carro.java
@@ -0,0 +1,13 @@
+package br.edu.up.modelos;
+
+class Carro {
+    private String modelo;
+    private String placa;
+    private String cor;
+
+    public Carro(String modelo, String placa, String cor) {
+        this.modelo = modelo;
+        this.placa = placa;
+        this.cor = cor;
+    }
+}
