6b29410668169cf5e256f8ae616b23afc0d33d32
Joao <jpjoaopedro201444@gmail.com>
2024-05-09 15:22:39 -0300

    Get/set Carro

--- Cenario04/src/br/edu/up/modelos/Carro.java
+++ Cenario04/src/br/edu/up/modelos/Carro.java
@@ -10,4 +10,28 @@ class Carro {
         this.placa = placa;
         this.cor = cor;
     }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getPlaca() {
+        return placa;
+    }
+
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
 }
