1d6c39b7ddde47010f910dd158ada5fcdda1c508
Pedro <pedromed.silva05@gmail.com>
2024-05-10 20:25:42 -0300

    Prompt Cenario02 + Correcao

--- Cenario02/src/br/edu/up/Programa.java
+++ Cenario02/src/br/edu/up/Programa.java
@@ -1,6 +1,7 @@
 package br.edu.up;
 
 import br.edu.up.modelos.Ponto;
+import br.edu.up.Prompt;
 
 public class Programa {
     public static void main(String[] args) {
@@ -10,15 +11,15 @@ public class Programa {
         Ponto ponto2 = new Ponto(2, 5);
 
         double Distancia = ponto1.calcularDistancia(ponto2);
-        System.out.println("A distancia entre o ponto1 e ponto2 é: " + Distancia);
+        Prompt.imprimir("A distancia entre o ponto1 e ponto2 é: " + Distancia);
 
         double DistanciaPonto2 = ponto2.calcularDistancia(7, 2);
-        System.out.println("A distancia entre o ponto2 a X: 7, Y:2 é: " + DistanciaPonto2);
+        Prompt.imprimir("A distancia entre o ponto2 a X: 7, Y:2 é: " + DistanciaPonto2);
 
         ponto1.setX(10);
         ponto1.setY(3);
 
         double novaDistancia = ponto1.calcularDistancia(ponto2);
-        System.out.println("A distancia entre o ponto1 e ponto2 é: " + novaDistancia);
+        Prompt.imprimir("A distancia entre o ponto1 e ponto2 é: " + novaDistancia);
     }
 }
