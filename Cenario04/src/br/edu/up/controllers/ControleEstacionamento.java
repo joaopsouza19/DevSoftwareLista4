b49cdb7f311608c4adf403ef48b6670cd412aa87
Pedro <pedromed.silva05@gmail.com>
2024-05-10 20:33:39 -0300

    Prompt Cenario04 + Correcao

--- Cenario04/src/br/edu/up/controllers/ControleEstacionamento.java
+++ Cenario04/src/br/edu/up/controllers/ControleEstacionamento.java
@@ -3,6 +3,7 @@ package br.edu.up.controllers;
 import java.util.ArrayList;
 import java.util.List;
 import br.edu.up.models.Carro;
+import br.edu.up.Prompt;
 
 public class ControleEstacionamento {
     private List<Carro> carrosEstacionados;
@@ -11,17 +12,17 @@ public class ControleEstacionamento {
 
     public ControleEstacionamento() {
         carrosEstacionados = new ArrayList<>();
-        vagasDisponiveis = 10; 
-        valorArrecadado = 0; 
+        vagasDisponiveis = 10;
+        valorArrecadado = 0;
     }
 
     public void entradaCarro(Carro carro) {
         if (vagasDisponiveis > 0) {
             carrosEstacionados.add(carro);
             vagasDisponiveis--;
-            System.out.println("O carro foi estacionado com sucesso!");
+            Prompt.imprimir("O carro foi estacionado com sucesso!");
         } else {
-            System.out.println("Não possui vagas disponíveis.");
+            Prompt.imprimir("Não possui vagas disponíveis.");
         }
     }
 
@@ -31,20 +32,20 @@ public class ControleEstacionamento {
             if (carro.getPlaca().equals(placa)) {
                 carrosEstacionados.remove(carro);
                 vagasDisponiveis++;
-                valorArrecadado += 5; 
+                valorArrecadado += 5;
                 encontrado = true;
-                System.out.println("O carro foi retirado com sucesso!");
+                Prompt.imprimir("O carro foi retirado com sucesso!");
                 break;
             }
         }
         if (!encontrado) {
-            System.out.println("O carro não foi encontrado no estacionamento.");
+            Prompt.imprimir("O carro não foi encontrado no estacionamento.");
         }
     }
 
     public void emitirRelatorio() {
-        System.out.println("O número de carros estacionados: " + carrosEstacionados.size());
-        System.out.println("O número de vagas disponíveis: " + vagasDisponiveis);
-        System.out.println("Valor arrecadado: R$ " + valorArrecadado);
+        Prompt.imprimir("O número de carros estacionados: " + carrosEstacionados.size());
+        Prompt.imprimir("O número de vagas disponíveis: " + vagasDisponiveis);
+        Prompt.imprimir("Valor arrecadado: R$ " + valorArrecadado);
     }
 }
