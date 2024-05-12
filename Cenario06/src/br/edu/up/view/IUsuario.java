08d04daa8f6768664a883a428c669eb5773f683c
Joao <jpjoaopedro201444@gmail.com>
2024-05-12 15:55:10 -0300

    Interface

---
+++ Cenario06/src/br/edu/up/view/IUsuario.java
@@ -0,0 +1,45 @@
+package br.edu.up.view;
+
+import java.util.List;
+import java.util.Scanner;
+
+public class IUsuario {
+    private Scanner scanner;
+
+    public IUsuario() {
+        this.scanner = new Scanner(System.in);
+    }
+
+    public void mostrarMenu() {
+        System.out.println("Selecione uma opção:");
+        System.out.println("1. Adicionar passageiro");
+        System.out.println("2. Adicionar comandante");
+        System.out.println("3. Adicionar comissário");
+        System.out.println("4. Adicionar aeronave");
+        System.out.println("5. Listar passageiros");
+        System.out.println("6. Listar tripulação");
+        System.out.println("7. Listar aeronaves");
+        System.out.println("8. Sair");
+    }
+
+    public String lerString(String prompt) {
+        System.out.print(prompt);
+        return scanner.nextLine();
+    }
+
+    public int lerInt(String prompt) {
+        System.out.print(prompt);
+        return Integer.parseInt(scanner.nextLine());
+    }
+
+    public void mostrarMensagem(String mensagem) {
+        System.out.println(mensagem);
+    }
+
+    public void mostrarLista(List<?> lista) {
+        for (Object item : lista) {
+            System.out.println(item);
+        }
+    }
+}
+
