c2ba2af552ff6eb5d0ed504bff3784f68a5746eb
Joao <pedromed.silva05@gmail.com>
2024-05-17 12:51:51 -0300

    Prompt Cenario10

--- Cenario10/src/br/edu/up/view/IUsuario.java
+++ Cenario10/src/br/edu/up/view/IUsuario.java
@@ -1,19 +1,20 @@
 package br.edu.up.view;
+import br.edu.up.Prompt;
 
 public class IUsuario {
     public void mostrarMenu(){
-        System.out.println("Menu:");
-        System.out.println("1. Incluir seguro");
-        System.out.println("2. Localizar seguro");
-        System.out.println("3. Excluir seguro");
-        System.out.println("4. Excluir todos os seguros");
-        System.out.println("5. Listar todos os seguros");
-        System.out.println("6. Ver quantidade de seguros");
-        System.out.println("7. Sair");
-        System.out.println("Escolha uma opção: ");
+        Prompt.imprimir("Menu:");
+        Prompt.imprimir("1. Incluir seguro");
+        Prompt.imprimir("2. Localizar seguro");
+        Prompt.imprimir("3. Excluir seguro");
+        Prompt.imprimir("4. Excluir todos os seguros");
+        Prompt.imprimir("5. Listar todos os seguros");
+        Prompt.imprimir("6. Ver quantidade de seguros");
+        Prompt.imprimir("7. Sair");
+        Prompt.imprimir("Escolha uma opção: ");
     }
 
     public void mostrarMensagem(String mensagem){
-        System.out.println(mensagem);
+        Prompt.imprimir(mensagem);
     }
 }
